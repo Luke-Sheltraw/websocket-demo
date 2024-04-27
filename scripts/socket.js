fc512a871f73638c9c91dda074db267ab035435a
Luke-Sheltraw <lukesheltraw@gmail.com>
2024-04-27 16:44:24 -0700

    init

---
+++ scripts/socket.js
@@ -0,0 +1,48 @@
+const WEB_SOCKET_URL = 'ws://localhost:8000';
+
+let webSocket;
+
+function sendMessage(messageContents) {
+  webSocket.send(messageContents);
+}
+
+function displayMessage(messageContents) {
+  const receivedMessageFeedEl = document.querySelector('#received_message_wrapper');
+
+  const newMessageEl = document.createElement('p');
+  newMessageEl.innerText = messageContents;
+
+  receivedMessageFeedEl.append(newMessageEl);
+}
+
+function initializeWebSocket() {
+  if (webSocket) {
+    console.log('WebSocket already initialized; quitting');
+    return;
+  }
+
+  webSocket = new WebSocket(WEB_SOCKET_URL, 'multiplayer-demo-protocol');
+
+  webSocket.addEventListener('open', () => {
+    webSocket.send('Open event received');
+  });
+
+  webSocket.addEventListener('message', (e) => {
+    console.log('Message receieved: ', e.data);
+    displayMessage(e.data);
+  })
+}
+
+function initializeChatListeners() {
+  const sendButtonEl = document.querySelector('button#send_message');
+  const messageContentsInputEl = document.querySelector('input#message_contents');
+
+  sendButtonEl.addEventListener('click', () => sendMessage(messageContentsInputEl.value) );
+}
+
+function init() {
+  initializeWebSocket();
+  initializeChatListeners();
+}
+
+window.addEventListener('DOMContentLoaded', init);
---
+++ server/main.js
@@ -0,0 +1,45 @@
+import { server } from 'websocket';
+import * as http from "node:http";
+
+const PORT = 8000;
+
+const httpServer = http.createServer();
+
+httpServer.listen(PORT);
+
+const webSocketServer = new server({
+  httpServer,
+  autoAcceptConnections: false,
+});
+
+console.log('Server online');
+
+let webSocketConnections = [];
+
+webSocketServer.on('request', (webSocketRequest) => {
+  console.log(`Request received at "${ webSocketRequest.remoteAddress }"`);
+
+  const webSocketConnection = webSocketRequest.accept('multiplayer-demo-protocol', webSocketRequest.origin);  
+
+  console.log(`WebSocket connected at "${ webSocketConnection.remoteAddress }"`);
+
+  webSocketConnections.push(webSocketConnection);
+
+  webSocketConnection.on('close', (code, desc) => {
+    console.log(`WebSocket disconnected at "${ webSocketConnection.remoteAddress }" with code "${ code }" and desc "${ desc }"`);
+    webSocketConnections = webSocketConnections.filter((con) => con !== webSocketConnection);
+  });
+
+  webSocketConnection.on('message', (data) => {
+    console.log(`Message received at "${ webSocketConnection.remoteAddress }": "${ data.utf8Data }"`);
+
+    webSocketConnections.forEach((webSocketConnection) => {
+      webSocketConnection.sendUTF(data.utf8Data);
+    })
+  })
+});
+
+
+webSocketServer.on('close', (webSocketConnection, closeReason, description) => {
+  console.log(description);
+});
