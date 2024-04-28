70983ecee587592de6b7bd6f883819a90b0097ef
Luke-Sheltraw <lukesheltraw@gmail.com>
2024-04-27 17:01:43 -0700

    updated socket

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,4 +1,4 @@
-const WEB_SOCKET_URL = 'wss://websocket-demo-sable.vercel.app:8000';
+const WEB_SOCKET_URL = 'wss://websocket-demo-luke.netlify.app:8000';
 // const WEB_SOCKET_URL = 'ws://localhost:8000';
 
 let webSocket;
