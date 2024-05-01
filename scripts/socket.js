429102e88ce1c3ff3241605aca71ecb96d32d5c1
Luke-Sheltraw <lukesheltraw@gmail.com>
2024-05-01 14:17:08 -0700

    added super basic multiplayer test

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,7 +1,10 @@
-const WEB_SOCKET_URL = 'wss://websocket-demo-6ygb.onrender.com';
-// const WEB_SOCKET_URL = 'ws://localhost:8000';
+// const WEB_SOCKET_URL = 'wss://websocket-demo-6ygb.onrender.com';
+const WEB_SOCKET_URL = 'ws://localhost:8000';
 
 let webSocket;
+const gameState = {
+  roomCode: null,
+};
 
 function sendMessage(messageContents) {
   webSocket.send(messageContents);
@@ -24,11 +27,23 @@ function initializeWebSocket() {
 
   webSocket = new WebSocket(WEB_SOCKET_URL, 'multiplayer-demo-protocol');
 
-  webSocket.addEventListener('open', () => {
-    webSocket.send('Open event received');
-  });
+  let gameJoined = false;
+
+  // webSocket.addEventListener('open', () => {
+  //   webSocket.send('Open event received');
+  // });
 
   webSocket.addEventListener('message', (e) => {
+    if (!gameJoined) {
+      console.log(JSON.parse(e.data));
+      const roomCode = JSON.parse(e.data).roomCode;
+      if (roomCode) {
+        gameJoined = true;
+        gameState.roomCode = roomCode;
+        document.querySelector('#user_code_contents').value = roomCode;
+      }
+    }
+
     console.log('Message receieved: ', e.data);
     displayMessage(e.data);
   })
@@ -39,6 +54,15 @@ function initializeChatListeners() {
   const messageContentsInputEl = document.querySelector('input#message_contents');
 
   sendButtonEl.addEventListener('click', () => sendMessage(messageContentsInputEl.value) );
+  messageContentsInputEl.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') sendMessage(messageContentsInputEl.value);
+  });
+
+  document.querySelector('button#join_code_button').addEventListener('click', () => {
+    const enteredCode = document.querySelector('input#join_code_contents').value;
+
+    sendMessage(JSON.stringify({ roomCode: enteredCode }));
+  });
 }
 
 function init() {
--- server/main.js
+++ server/main.js
@@ -18,12 +18,22 @@ console.log(webSocketServer);
 
 let webSocketConnections = [];
 
+const gameInstances = {};
+
 webSocketServer.on('request', (webSocketRequest) => {
   console.log(`Request received at "${ webSocketRequest.remoteAddress }"`);
 
   const webSocketConnection = webSocketRequest.accept('multiplayer-demo-protocol', webSocketRequest.origin);  
 
   console.log(`WebSocket connected at "${ webSocketConnection.remoteAddress }"`);
+
+  const roomCode = Math.floor(Math.random() * 10000);
+
+  webSocketConnection.sendUTF(JSON.stringify({ roomCode }));
+  gameInstances[roomCode] = {
+    roomCode,
+    adminPlayerConnection: webSocketConnection,
+  }
 
   webSocketConnections.push(webSocketConnection);
 
@@ -35,9 +45,16 @@ webSocketServer.on('request', (webSocketRequest) => {
   webSocketConnection.on('message', (data) => {
     console.log(`Message received at "${ webSocketConnection.remoteAddress }": "${ data.utf8Data }"`);
 
-    webSocketConnections.forEach((webSocketConnection) => {
-      webSocketConnection.sendUTF(data.utf8Data);
-    })
+    
+    const roomCode = JSON.parse(data.utf8Data).roomCode;
+    if (roomCode) {
+      gameInstances[roomCode].secondaryPlayerConnection = webSocketConnection;
+    }
+    
+    console.log(gameInstances);
+    // webSocketConnections.forEach((webSocketConnection) => {
+    //   webSocketConnection.sendUTF(data.utf8Data);
+    // })
   })
 });
 
